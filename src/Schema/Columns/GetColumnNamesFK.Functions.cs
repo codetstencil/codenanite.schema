944d2547318aa1a1937961e445249bf7e4568a40
Ayo Dahunsi <ayo@dolasoft.com>
2020-12-14 04:50:05 -0700

    GetColumnNamesFK: Added function to get the list of Foreign Key Column Names

---
+++ src/Schema/Columns/GetColumnNamesFK.Functions.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace ZeraSystems.CodeNanite.Schema
+{
+    public partial class GetColumnNamesFK
+    {
+        private string _table;
+        private void MainFunction()
+        {
+            _table = GetTable(Input);
+            OutputList = GetColumns(_table)
+                .Where(e => !string.IsNullOrEmpty(e.ColumnType))
+                .Where(t => t.IsChecked)
+                .Where(t => t.IsForeignKey)
+                .Select(s=>s.ColumnName)
+                .ToList();
+        }
+    }
+}
---
+++ src/Schema/Columns/GetColumnNamesFK.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using ZeraSystems.CodeNanite.Expansion;
+using ZeraSystems.CodeStencil.Contracts;
+
+namespace ZeraSystems.CodeNanite.Schema
+{
+    /// <summary>
+    /// There are 6 elements in the String Array used by the
+    /// 0 - Publisher : This is the name of the publisher
+    /// 1 - Title : This is the title of the Code Nanite
+    /// 2 - Details : This is the
+    /// 3 - Version Number
+    /// 4 - Label : Label of the Code Nanite
+    /// 5 - Namespace
+    /// 6 - Release Date
+    /// 7 - Name to use for Expander Label
+    /// 8 - Indicates that the Nanite is Schema Dependent
+    /// 9 - RESERVED
+    /// 10 - RESERVED
+    /// </summary>
+    [Export(typeof(ICodeStencilCodeNanite))]
+    [CodeStencilCodeNanite(new[]
+    {
+        "Zera Systems Inc.",                        // 0 - Publisher : This is the name of the publisher
+        "Get the list of Foreign Key Column Names", // 1 - Title : This is the title of the Code Nanite
+        "Returns a list of Foreign Key Column " +
+        "Name of the passed table.",               // 2
+        "1.0",                                      // 3
+        "GetColumnNamesFK",                         // 4
+        "ZeraSystems.CodeNanite.Schema",            // 5
+        "12/13/2020",                               // 6
+        "CS_COLUMN_NAMES_FK",                       // 7
+        "1",                                        // 8
+        "",                                         // 9
+        "https://codestencil.com/documentation/how-codestencil-works"   // 10
+    })]
+    public partial class GetColumnNamesFK : ExpansionBase, ICodeStencilCodeNanite
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+        public int Counter { get; set; }
+        public List<string> OutputList { get; set; }
+        public List<ISchemaItem> SchemaItem { get; set; }
+        public List<IExpander> Expander { get; set; }
+        public List<string> InputList { get; set; }
+
+        public void ExecutePlugin()
+        {
+            Initializer(SchemaItem, Expander);
+            MainFunction();
+            //Output = ExpandedText.ToString();
+        }
+    }
+}
